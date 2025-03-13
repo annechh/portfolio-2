4d31e617c8b37f17157c00360ec1a0d1f0b9c419
annechh <142426482+annechh@users.noreply.github.com>
2025-03-13 15:38:32 +0100

    Set up basic Routes with Layout and Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+
 import './index.css';
+import Home from './pages/Home';
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
