c79fd012629beea45ca27006910ca1647a9b685c
annechh <142426482+annechh@users.noreply.github.com>
2025-03-14 18:02:32 +0100

    Import Projects component and image to add to Projects

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,19 @@
+import Projects from '../components/Projects';
+import bgImage from '../assets/ramsdalsnipa.jpg';
+import Background from '../components/Background';
+import SA from '../assets/SA.png';
+import SCM from '../assets/SCM.png';
+import SE from '../assets/SE.png';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="h-screen flex flex-col justify-center items-center">
+      <Background props={bgImage} />
+      <div className="card-wrapper grid grid-cols-3 gap-5">
+        <Projects props={SA} />
+        <Projects props={SCM} />
+        <Projects props={SE} />
+      </div>
     </div>
   );
 }
