8a2a19fd3f1ccd51e2ec2a73509a29f882698ea7
annechh <142426482+annechh@users.noreply.github.com>
2025-03-14 17:59:22 +0100

    Adds Header component, includes logo and some styles

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import logo from '../assets/HaugeDevLogo.svg';
+
+export default function Header() {
+  return (
+    <header className="fixed w-screen px-10">
+      <div className="flex justify-between items-center">
+        <img src={logo} alt="" />
+        <nav className="flex gap-5">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
