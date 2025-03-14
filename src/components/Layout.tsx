0a892cc271715692e2a0da3370018b252f5cd7ba
annechh <142426482+annechh@users.noreply.github.com>
2025-03-14 17:58:32 +0100

    Import Header and Footer component and added some styles for Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 export default function Layout() {
   return (
     <>
-      <div>
-        <main>
+      <div className="font-display text-white flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
           <Outlet />
         </main>
+        <Footer />
       </div>
     </>
   );
