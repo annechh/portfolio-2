fe49f326a2a335dacc12eaf5f3b5a4a2564a162d
annechh <142426482+annechh@users.noreply.github.com>
2025-03-14 17:57:02 +0100

    Adds Footer component and some styles

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-center w-screen py-5 text-sm tracking-[3.2px] font-light">
+      <p>Copyright @ Hauge Dev</p>
+    </footer>
+  );
+}
