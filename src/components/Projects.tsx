28acb06c031691ceb876faaf8ead2aa73c730d9d
Anne <142426482+annechh@users.noreply.github.com>
2025-03-17 10:53:00 +0100

    Change: updated handle props and paragraph for description

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,27 @@
-type cardImage = {
-  props: string;
+type Props = {
+  image: string;
+  title: string;
+  description: string;
 };
 
-export default function Projects({ props }: cardImage) {
+const Projects: React.FC<Props> = ({ image, title, description }) => {
   return (
     <div>
       <div className="card">
         <div className="img-container">
-          <img src={props} alt="Background photo" />
+          <img src={image} alt="Background photo" />
+        </div>
+        <div className="info-container">
+          <h3>{title}</h3>
+          <div className="description-container flex flex-col gap-5">
+            {description.split('\n').map((paragraph, index) => (
+              <p key={index}>{paragraph}</p>
+            ))}
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Projects;
