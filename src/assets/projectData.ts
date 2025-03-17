5d3a4975f2b172020c594ca2e7d6827d5158673b
Anne <142426482+annechh@users.noreply.github.com>
2025-03-17 10:50:53 +0100

    Adds: my project data info

---
+++ src/assets/projectData.ts
@@ -0,0 +1,29 @@
+import SA from '../assets/SA.png';
+import SCM from '../assets/SCM.png';
+import SE from '../assets/SE.png';
+
+export const projectData = [
+  {
+    id: 1,
+    image: SA,
+    title: 'Shira´s Adventure',
+    description: `First year Exam Project. In this project we were creating a blog page using Api. The methods we were to use were CREATE, GET, EDIT, PUT and DELETE. 
+      We could create our own fictional client, so I chose my dog Shira to be my client. I had a lot of fun with this project since I used Shira as my client, and could provide my own photos.`,
+  },
+
+  {
+    id: 2,
+    image: SCM,
+    title: 'Sunnfjord Science Museum',
+    description: `Design and build a modern, accessible and responsive web presence for the client using the provided image and text content.
+    The site should appeal to youngsters without pandering; while aiming at a younger audience it should not patronize them with overtly childish themes. The core purpose of the site is to excite and entice people to visit the museum.`,
+  },
+
+  {
+    id: 3,
+    image: SE,
+    title: 'Square Eyes',
+    description: `SquareEyes was my first assignment to put into practice what I’ve learned about HTML and CSS over this course by bringing my design alive.
+    SquareEyes is a video streaming website where you can purchase high-quality videos directly from the people who make them.`,
+  },
+];
