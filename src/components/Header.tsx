c9f9202f9289d7137acb46bb5e286de96b4536c3
Anne <142426482+annechh@users.noreply.github.com>
2025-03-17 10:51:25 +0100

    Style: adds a few styles to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import logo from '../assets/HaugeDevLogo.svg';
 
 export default function Header() {
   return (
-    <header className="fixed w-screen px-10">
+    <header className="fixed w-screen px-5 md:px-10">
       <div className="flex justify-between items-center">
-        <img src={logo} alt="" />
+        <img src={logo} alt="logo" className="h-12" />
         <nav className="flex gap-5">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
