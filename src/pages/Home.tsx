58451915e6a05e37dcca8c959b4da561f44f79f0
Anne <142426482+annechh@users.noreply.github.com>
2025-03-17 10:49:38 +0100

    Change: update using data from projectData

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,23 @@
 import Projects from '../components/Projects';
+import { projectData } from '../assets/projectData';
 import bgImage from '../assets/ramsdalsnipa.jpg';
 import Background from '../components/Background';
-import SA from '../assets/SA.png';
-import SCM from '../assets/SCM.png';
-import SE from '../assets/SE.png';
 
 export default function Home() {
+  console.log(projectData);
+
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center">
       <Background props={bgImage} />
-      <div className="card-wrapper grid grid-cols-3 gap-5">
-        <Projects props={SA} />
-        <Projects props={SCM} />
-        <Projects props={SE} />
+      <div className="card-wrapper h-screen grid grid-cols-1 gap-5 my-[100px]">
+        {projectData.map((project) => (
+          <Projects
+            key={project.id}
+            image={project.image}
+            title={project.title}
+            description={project.description}
+          />
+        ))}
       </div>
     </div>
   );
