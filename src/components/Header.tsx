348e5d011fb37a0b98798bf60b6082e8bfa316dc
annechh <142426482+annechh@users.noreply.github.com>
2025-03-19 14:28:07 +0100

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,10 @@ export default function Header() {
     <header className="fixed w-screen px-5 md:px-10">
       <div className="flex justify-between items-center">
         <img src={logo} alt="logo" className="h-12" />
-        <nav className="flex gap-5">
-          <Link to="/">Home</Link>
+        <nav className="flex gap-5 ">
+          <Link className="text-sm" to="/">
+            Home
+          </Link>
           <Link to="/about">About</Link>
         </nav>
       </div>
