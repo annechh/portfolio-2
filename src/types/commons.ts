5ca400265a324c24abebe2bbdc89748d54c563b1
annechh <142426482+annechh@users.noreply.github.com>
2025-04-30 01:15:40 +0200

    update: common.ts type

--- src/types/commons.ts
+++ src/types/commons.ts
@@ -3,9 +3,17 @@ export type MediaType = {
   alt: string;
 };
 
+export type LinkType = {
+  link: string;
+};
+
 export type ProjectType = {
   id: number;
   media: MediaType;
   title: string;
   description: string;
+  icons: MediaType;
+  githubMedia: MediaType;
+  githubLink: LinkType;
+  liveDemo: LinkType;
 };
