831c7a4936709617b8aa9b6b1dacb35c88561dc6
Anne <142426482+annechh@users.noreply.github.com>
2025-05-25 18:34:06 +0200

    Update footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="flex justify-center w-screen py-5 text-xs">
-      <p>Copyright @ Hauge Dev</p>
+      <p>Copyright © Hauge Dev</p>
     </footer>
   );
 }
