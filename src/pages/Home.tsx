7e5d3ed58e358ce652fce3144f3f75d10d9be5f2
Anne <142426482+annechh@users.noreply.github.com>
2025-05-25 21:33:06 +0200

    Style: minor style changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,11 @@ import Background from '../components/Background';
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center max-w-[1920px]">
       <Background image={backgroundData} />
-      <section className="my-[100px]">
-        <h2 className="font-bold text-5xl text-shadow-[] ">Projects</h2>
-        <div className="card-wrapper grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 mx-10">
+      <section className="my-[100px] mx-10 flex flex-col gap-5">
+        <h2 className="font-bold text-3xl text-shadow-[] ">My Projects</h2>
+        <div className="card-wrapper grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 ">
           {projectData.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
