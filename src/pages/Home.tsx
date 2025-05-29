175b09dea7f9c614c246b44dadddc83d7afd415f
Anne <142426482+annechh@users.noreply.github.com>
2025-05-29 15:45:28 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
       <section className="flex flex-col justify-center items-center gap-3 text-center px-5">
         <h1 className="text-4xl md:text-5xl font-bold ">Page is under production</h1>
-        <p>Expected to be finished within June 8, 2025</p>
+        <p>Expected to be finished by June 8, 2025</p>
         <img src={LOGO_ICON} alt="HaugeDev logo icon" className="spin" />
       </section>
     </div>
