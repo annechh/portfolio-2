12d757789011c08d4d6b14cfd92333391b58ecd9
annechh <142426482+annechh@users.noreply.github.com>
2025-05-29 14:44:54 +0200

    display message, under production

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,9 +12,9 @@ export default function Header() {
             <Link to="/">Home</Link>
           </li>
 
-          <li>
+          {/* <li>
             <Link to="/about">About</Link>
-          </li>
+          </li> */}
         </ul>
       </nav>
     </header>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,54 +1,17 @@
-import ProjectCard from '../components/ProjectCard';
-import { backgroundData, projectData } from '../assets/projectData';
+import { backgroundData } from '../assets/projectData';
 import Background from '../components/Background';
-import InfoCard from '../components/InfoCard';
+import LOGO_ICON from '../assets/images/LogoIcon.svg';
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center w-full">
       <Background image={backgroundData} />
 
-      <section className="min-h-screen flex flex-col justify-center items-center bg-pink-400">
-        <InfoCard />
-      </section>
-
-      <button className="max-w-[250px] px-10 py-3.5 rounded-full bg-purple-700">Test button</button>
-
-      <section className="my-[100px] mx-10 flex flex-col justify-center items-center w-full gap-5 min-h-screen bg-deep-indigo">
-        <h2 className="font-bold text-3xl text-shadow-[] ">My Projects</h2>
-        <div className="card-wrapper gap-5 max-w-[1920px]">
-          {projectData.map((project) => (
-            <ProjectCard key={project.id} project={project} />
-          ))}
-        </div>
-      </section>
-
-      <section className="min-h-screen flex flex-col justify-center items-center bg-green-400">
-        <InfoCard />
+      <section className="flex flex-col justify-center items-center gap-3 text-center px-5">
+        <h1 className="text-4xl md:text-5xl font-bold ">Page is under production</h1>
+        <p>Expected to be finished within June 8, 2025</p>
+        <img src={LOGO_ICON} alt="HaugeDev logo icon" className="spin" />
       </section>
     </div>
   );
 }
-
-// <div className="flex flex-col justify-center items-center max-w-[1920px]">
-//   <Background image={backgroundData} />
-
-//   <section className="min-h-screen flex flex-col justify-center items-center bg-pink-400">
-//     <InfoCard />
-//   </section>
-
-//   <button className="max-w-[250px] px-10 py-3.5 rounded-full bg-purple-700">Test button</button>
-
-//   <section className="my-[100px] mx-10 flex flex-col gap-5 min-h-screen">
-//     <h2 className="font-bold text-3xl text-shadow-[] ">My Projects</h2>
-//     <div className="card-wrapper grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 ">
-//       {projectData.map((project) => (
-//         <ProjectCard key={project.id} project={project} />
-//       ))}
-//     </div>
-//   </section>
-
-//   <section className="min-h-screen flex flex-col justify-center items-center bg-green-400">
-//     <InfoCard />
-//   </section>
-// </div>
