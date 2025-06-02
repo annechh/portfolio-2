cd30848d10150ece3dcc29529404d2bd75334468
annechh <142426482+annechh@users.noreply.github.com>
2025-06-02 17:53:38 +0200

    update home with introduction, projects, about me

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,50 @@
-import ProjectCard from '../components/ProjectCard';
-import { backgroundData, projectData } from '../assets/projectData';
-import Background from '../components/Background';
-// import InfoCard from '../components/InfoCard';
-import Carousel from '../components/Carousel';
+import { aboutMeData } from '../assets/data';
+import { projectData } from '../assets/projectData';
+
+// import Background from '../components/Background';
 import Introduction from '../components/Introduction';
+import ProjectCards from '../components/ProjectCards';
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center w-full">
-      <Background image={backgroundData} />
+      {/* <Background image={backgroundData} /> */}
 
       <Introduction />
 
-      <button className="max-w-[250px] px-10 py-3.5 rounded-full bg-purple-700">Test button</button>
-
-      <section className="my-[100px] flex flex-col justify-center items-center w-full min-h-screen bg-deep-indigo">
-        <div className="flex max-w-[1000px] bg-amber-300"></div>
-      </section>
-
-      <section className="flex flex-col justify-center items-center w-full h-screen">
-        <h2 className="text-3xl lg:text-5xl tracking-widest font-bold">My projects</h2>
-        <Carousel />
-      </section>
-
-      <section className="my-[100px] mx-10 flex flex-col justify-center items-center w-full gap-5 min-h-screen bg-deep-indigo">
-        <h2 className="font-bold text-3xl">My Projects</h2>
-        <div className="card-wrapper gap-5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 max-w-[1920px] px-5">
+      <section className="flex flex-col justify-center items-center w-full min-h-screen bg-[#1d1d1d] py-16">
+        <h2 className="h-14 text-[40px] font-bold text-pink w-full max-w-[300px] md:max-w-[624px] lg:max-w-[948px] xl:max-w-[1098px] 2xl:max-w-[1260px]">
+          MY PROJECTS
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-[1260px] border-t py-6">
           {projectData.map((project) => (
-            <ProjectCard key={project.id} project={project} />
+            <ProjectCards key={project.id} project={project} />
           ))}
         </div>
       </section>
 
-      <section className="min-h-screen flex flex-col justify-center items-center bg-green-400">
-        {/* <InfoCard /> */}
+      <section className="min-h-screen flex flex-col justify-center items-center w-full bg-dark">
+        <h2 className="h-14 text-[40px] font-bold text-pink w-full max-w-[1260px] px-6">
+          ABOUT ME
+        </h2>
+        <div className="flex max-w-[1260px] h-full w-full border-y px-6">
+          {/* <div className="flex flex-col self-end h-full"> */}
+          <div className="flex flex-col self-end max-w-[500px] w-full h-full">
+            <img
+              src={aboutMeData.media.url}
+              alt={aboutMeData.media.alt}
+              className="w-full h-full object-cover"
+            />
+          </div>
+          {/* </div> */}
+          <div className="max-w-[740px] w-full p-6">
+            {aboutMeData.description.split('\n\n').map((para, index) => (
+              <p key={index} className="lg:text-xl mb-4">
+                {para}
+              </p>
+            ))}
+          </div>
+        </div>
       </section>
     </div>
   );
