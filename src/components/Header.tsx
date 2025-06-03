b8e5eb5f36425f8de15cd4aafe8ec5a4ca995d60
annechh <142426482+annechh@users.noreply.github.com>
2025-06-03 17:48:28 +0200

    style: update header height and font sizes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,11 @@ import logo from '../assets/images/HaugeDevLogo.svg';
 
 export default function Header() {
   return (
-    <header className="fixed flex w-full h-24 px-5 md:px-10 backdrop-blur-[20px] z-50">
+    <header className="fixed flex w-full h-[60px] lg:h-[100px] px-5 md:px-10 backdrop-blur-[20px] z-50">
       <nav className="flex justify-between items-center w-full gap-5 text-sm ">
-        <img src={logo} alt="logo" className="h-[80px] w-[150px]" />
+        <img src={logo} alt="logo" className="h-[40px] w-[75px] lg:h-[80px] lg:w-[150px]" />
 
-        <ul className="flex gap-5">
+        <ul className="flex gap-5 text-base lg:text-lg">
           <li>
             <Link to="/">Home</Link>
           </li>
