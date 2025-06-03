99fb2603953338c881c13db67304e4a7a2c2d552
annechh <142426482+annechh@users.noreply.github.com>
2025-06-03 17:11:51 +0200

    style: update footer border style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center w-full h-[50px] text-xs fixed bottom-0 border-thin backdrop-blur-[20px] z-50">
+    <footer className="flex justify-center items-center w-full h-[50px] text-xs fixed bottom-0 border-t border-white-faded backdrop-blur-[20px] z-50">
       <p>Copyright © Hauge Dev</p>
     </footer>
   );
