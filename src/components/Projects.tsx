27c03c04aa8e88cce0da8afe0b0816e54f921a63
annechh <142426482+annechh@users.noreply.github.com>
2025-06-07 01:29:53 +0200

    removed shadow on project and tech title

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,9 +7,7 @@ const Projects = () => {
       id="projects"
       className="relative flex flex-col justify-center items-center w-full min-h-screen px-6 py-[70px] lg:py-[150px]"
     >
-      <h2 className="pb-2 text-2xl lg:text-[40px] font-bold w-full max-w-[1260px] text-shadow-sm">
-        PROJECTS
-      </h2>
+      <h2 className="pb-2 text-2xl lg:text-[40px] font-bold w-full max-w-[1260px] ">PROJECTS</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full gap-6 max-w-[1260px] border-t border-white/50 py-6">
         {projectData.map((project) => (
           <ProjectCards key={project.id} project={project} />
--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -11,9 +11,7 @@ const Tech = () => {
       transition={{ duration: 0.5 }}
       className="relative min-h-screen flex flex-col justify-center items-center w-full px-6 py-[70px] lg:py-[150px]"
     >
-      <h2 className="pb-2 text-2xl lg:text-[40px] font-bold w-full max-w-[1260px] text-shadow-sm">
-        Tech
-      </h2>
+      <h2 className="pb-2 text-2xl lg:text-[40px] font-bold w-full max-w-[1260px]">Tech</h2>
       <div className="flex flex-col max-w-[1260px] h-full w-full border-t border-white/50 mb-[100px]">
         <ul className="flex justify-center gap-10 flex-wrap py-10">
           {techData.map((tech) => (
