ec699a5c8f0a8c2bd6a7914e48b5ecaf6dd6a304
annechh <142426482+annechh@users.noreply.github.com>
2025-06-06 19:20:55 +0200

    updated data

---
+++ src/data/aboutData.ts
@@ -0,0 +1,31 @@
+import image_1 from '../assets/images/Anne1.png';
+import image_2 from '../assets/images/Anne2.png';
+
+export const introductionData = {
+  title: 'Designing, building and always learning',
+  intro1: `Hello, I'm Anne`,
+  intro2: 'From Figma to functional – bringing interfaces to life.',
+  media: {
+    url: image_1,
+    alt: 'Portrait',
+  },
+};
+
+export const aboutData = {
+  title: 'About Me',
+  description: `
+  Hi! 👋 
+  I’m Anne-Cathrine Hauge, a 36-year-old front-end developer from Naustdal, Norway.
+
+  I’m finishing a two-year front-end education at Noroff, working with HTML, CSS, JavaScript, React, and design tools like Figma. I enjoy turning visual ideas into clean, user-friendly websites.
+
+  What excites me most is constant learning and creative problem-solving. My goal is to join a team where I can grow, contribute, and eventually explore backend development to become a full-stack developer.
+
+  When not coding, you’ll find me hiking, snowboarding, paddling, or relaxing indoors with gaming, movies, or series.
+
+  Feel free to reach out to connect or collaborate!`,
+  media: {
+    url: image_2,
+    alt: 'Girl in snowboard clothes with arms spread wide, smiling at the camera behind a balaclava mask and goggles',
+  },
+};
